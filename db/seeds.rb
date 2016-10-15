d48e5300e676448da641019060d372e4dba35418
Bryce Kerley <bkerley@brycekerley.net>
2016-10-15 14:16:45 -0400

    start chat channel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chat_lines = <<-EOF
+You too, thanks.
+Mm-hm.
+Sure.
+Yeah.
+Nice!
+Cool.
+Sounds interesting.
+*smiles without saying anything*
+EOF
+
+chat_lines.lines.map(&:chomp).each do |txt|
+  Response.where(body: txt).first_or_create
+end
