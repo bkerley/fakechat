47b631e7b54a1c58ad9dab0766eb179bb7ab11ca
Bryce Kerley <bkerley@brycekerley.net>
2016-10-15 14:27:32 -0400

    working-ish without sockets

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,11 @@
 class ChatsController < ApplicationController
   def index
   end
+
+  def create
+    @was_said = params[:chat]
+    @reply = Response.order('random()').first.body
+
+    render action: 'index'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'chats/index'
+  root 'chats#index'
 
+  resources :chats
   resources :responses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
